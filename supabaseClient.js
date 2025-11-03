9a1ad60d7a94c4da0825862c0773e4ce4f8111d4
Luz Angela Rodriguez <luzangelarodriguez3@gmail.com>
2025-11-03 05:38:49 -0500

    desacerlo de esconder las claves  en workers

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,17 +1,6 @@
-window.SB_API = 'https://styletime-proxy.luzangelarodriguez3.workers.dev';
+// supabaseClient.js
+window.SB_URL = 'https://jzzdjhxisuuwgajujduu.supabase.co';
+window.SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6emRqaHhpc3V1d2dhanVqZHV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2NjUwOTMsImV4cCI6MjA3NTI0MTA5M30.gtzWWZwZGkpjyLvbH9M3HAs5xtQ1mLcDFnmT25-1F5s';
 
-// El cliente ya no necesita las claves directamente,
-// ahora todas las peticiones pasarán por el Worker
-window.sb = {
-  async from(table) {
-    return {
-      select: async (query = '*') => {
-        const res = await fetch(`${window.SB_API}/rest/v1/${table}?select=${query}`);
-        const data = await res.json();
-        return { data, error: null };
-      }
-    };
-  }
-};
-
-
+// @supabase/supabase-js de la CDN expone "supabase" en window
+window.sb = supabase.createClient(window.SB_URL, window.SB_KEY);
